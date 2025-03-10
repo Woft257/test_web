2fba7faf983bc414c508fcef3836df805b6b8322
Woft257 <h.hoangtuan257@gmail.com>
2025-03-10 21:15:41 +0700

    Add IPFS gateway support to remote image patterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "api.opensea.io",
-          pathname: "/api/v1/asset/**",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.opensea.io",
+        pathname: "/api/v1/asset/**",
+      },
+      // Thêm các gateway IPFS sau
+      {
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+        pathname: "/ipfs/**",
+      }
+    ],
+  },
   env: {
     ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
   },
 };
-  
 
 export default nextConfig;
